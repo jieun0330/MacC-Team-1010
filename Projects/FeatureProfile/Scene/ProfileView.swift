ac410279a1953a702db735b1b310dfc35a9a378b
sunghun <leedool3003@gmail.com>
2023-10-13 13:59:24 +0900

    :hammer: [chore] FeatureProfile 모듈 추가

---
+++ Projects/FeatureProfile/Scene/ProfileView.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileView.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Kim SungHun on 2023/10/12.
+//
+
+import SwiftUI
+
+public struct ProfileView: View {
+	
+	public init() {}
+	
+	public var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct ProfileView_Previews: PreviewProvider {
+	static var previews: some View {
+		ProfileView()
+	}
+}
