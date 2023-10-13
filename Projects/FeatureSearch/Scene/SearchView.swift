990894448c67cab155eac445ccaae96856ce2114
sunghun <leedool3003@gmail.com>
2023-10-13 14:00:01 +0900

    :hammer: [chore] FeatureSearch 모듈 추가

---
+++ Projects/FeatureSearch/Scene/SearchView.swift
@@ -0,0 +1,23 @@
+//
+//  SearchView.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Kim SungHun on 2023/10/12.
+//
+
+import SwiftUI
+
+public struct SearchView: View {
+	
+	public init() {}
+	
+	public var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct SearchView_Previews: PreviewProvider {
+	static var previews: some View {
+		SearchView()
+	}
+}
