2ee0786e3796cc64b07ae92a679345e9fb7f74ab
sunghun <leedool3003@gmail.com>
2023-10-18 16:32:48 +0900

    :hammer: [chore] mock modle 수정 #4

--- Projects/Core/Sources/Models/MockModel.swift
+++ Projects/Core/Sources/Models/MockModel.swift
@@ -8,11 +8,27 @@
 
 import Foundation
 
-public enum MockTaste: String {
-	case sweet = "단맛"
-	case bitter = "쓴맛"
-	case refreshing = "청량감"
-	case mild = "걸쭉함"
+public enum MockTaste {
+	case sweet
+	case bitter
+	case refreshing
+	case mild
+	
+	public var description: String {
+		switch self {
+		case .sweet:
+			return "단맛"
+			
+		case .bitter:
+			return "쓴맛"
+			
+		case .refreshing:
+			return "청량감"
+			
+		case .mild:
+			return "걸쭉함"
+		}
+	}
 }
 
 public struct MockMakgeolliModel: Hashable {
@@ -21,7 +37,8 @@ public struct MockMakgeolliModel: Hashable {
 	public var alcoholContent: Double = 16.0 // 도수
 	public var capacity: Int = 750 // 용량
 	public var price: Int = 150000 // 가격
-	public var taste: [MockTaste] = [.bitter, .mild] // 맛에 대한 설명 배열
+	public var taste: [MockTaste] = [.sweet, .bitter, .refreshing, .mild] // 맛에 대한 설명 배열
+	public var tasteScore: [Int] = [0, 0, 0, 0] // 맛에 대한 점수 배열
 	public var info: String = "막걸리 정보" // 정보
 	public var awards: [String] = ["2023 대상"] // 수상 정보 배열
 	public var ingredients: [String] = ["쌀", "물"] // 원재료 배열
