967442e3aab1b7902d7c142293fe93ac94c88581
sunghun <leedool3003@gmail.com>
2023-10-18 23:38:41 +0900

    :hammer: [chore] 지역 카테고리 set model #6

---
+++ Projects/Core/Sources/Models/Region/RegionType.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  Core
+//
+//  Created by Kim SungHun on 2023/10/18.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import Foundation
+
+public enum RegionType: String, CaseIterable, Hashable {
+	// 추후 변경
+	case 강원
+	case 충북
+	case 충남
+	case 제주
+	case 울릉
+	case 경북
+	case 경남
+	case 경기
+	case 서울
+}
