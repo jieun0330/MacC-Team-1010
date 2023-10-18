276b5627c5f93791603ee44f234c6f25200fb3bb
sunghun <leedool3003@gmail.com>
2023-10-18 22:39:35 +0900

    :lipstick: [UI] 특징으로 찾기 color 추가 #6

--- Projects/DesignSystem/Sources/Colors/Color.swift
+++ Projects/DesignSystem/Sources/Colors/Color.swift
@@ -9,10 +9,26 @@
 public enum TENTENColor {
 	case bgColor
 	
+	case additiveColor
+	case alcoholColor
+	case refreshColor
+	case sourColor
+	case sweetColor
+	case thickColor
+	
+	case tempGrayColor
+	
 	/// 컬러를 불러오기 위한 name space
 	public var name: String {
 		switch self {
 		case .bgColor: return "bgColor"
+		case .additiveColor: return "additiveColor"
+		case .alcoholColor: return "alcoholColor"
+		case .refreshColor: return "refreshColor"
+		case .sourColor: return "sourColor"
+		case .sweetColor: return "sweetColor"
+		case .thickColor: return "thickColor"
+		case .tempGrayColor: return "tempGrayColor"
 		}
 	}
 }
