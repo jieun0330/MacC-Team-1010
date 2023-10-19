0f666a78cc0a9b08400ae28312066ddd4219174f
sunghun <leedool3003@gmail.com>
2023-10-20 01:05:37 +0900

    :lipstick: [UI] add custom navi, back button #8

---
+++ Projects/Common/Sources/Componets/CustomBackButton.swift
@@ -0,0 +1,41 @@
+//
+//  CustomBackButton.swift
+//  Common
+//
+//  Created by Kim SungHun on 2023/10/20.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import SwiftUI
+import DesignSystem
+
+public struct CustomBackButton: View {
+	@Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+	
+	let title: String
+	
+	public init(title: String = "뒤로가기") { self.title = title }
+	
+	public var body: some View {
+		Button(action: {
+			self.presentationMode.wrappedValue.dismiss()
+		}) {
+			HStack(spacing: 4) {
+				Image(systemName: "chevron.left")
+				Text(title)
+			}
+			.foregroundColor(Color(uiColor: .designSystem(.tempBackButtonGrayColor)!))
+		}
+	}
+}
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+	override open func viewDidLoad() {
+		super.viewDidLoad()
+		interactivePopGestureRecognizer?.delegate = self
+	}
+	
+	public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+		return viewControllers.count > 1
+	}
+}
