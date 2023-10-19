e55d8c1be9c9ea80c6a1dfc6926807672f3026c1
sunghun <leedool3003@gmail.com>
2023-10-19 09:28:41 +0900

    :sparkles: [feat] 가격대별로 찾기 뷰 추가 #6

---
+++ Projects/FeatureMain/Scene/Price/PriceView.swift
@@ -0,0 +1,55 @@
+//
+//  PriceView.swift
+//  FeatureMain
+//
+//  Created by Kim SungHun on 2023/10/18.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import SwiftUI
+import Core
+
+struct PriceView: View {
+	
+	private let priceGroup = stride(from: 0,
+									to: PriceType.allCases.count,
+									by: 2).map { startIndex in
+		Array(PriceType.allCases[
+			startIndex..<min(startIndex + 2, PriceType.allCases.count)
+		])
+	}
+	
+	var body: some View {
+		VStack {
+			HStack(spacing: 0) {
+				Text("가격대별")
+					.font(.system(size: 18, weight: .bold))
+					.foregroundColor(.white)
+				Text("로 찾기")
+					.font(.system(size: 18, weight: .regular))
+					.foregroundColor(.white)
+				Spacer()
+				Image(systemName: "chevron.right")
+			}
+			Spacer()
+				.frame(height: 16)
+			ScrollView(.horizontal, showsIndicators: false) {
+				ForEach(priceGroup, id: \.self) { prices in
+					HStack {
+						ForEach(prices, id: \.self) { price in
+							PriceSignleView(title: price.description)
+								.frame(width: 200)
+						}
+					}
+				}
+			}
+		}
+		.padding(.leading, 16)
+	}
+}
+
+struct PriceView_Previews: PreviewProvider {
+	static var previews: some View {
+		PriceView()
+	}
+}
