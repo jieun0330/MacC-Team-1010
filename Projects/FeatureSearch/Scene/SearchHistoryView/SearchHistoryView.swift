07016f8a227aecaec0bc7e4d8954a423331211fe
mininim <minsoplee2015@gmail.com>
2023-10-20 16:17:52 +0900

    :sparkles: [feat] SearchSuggestionView 기초 구현 #9

---
+++ Projects/FeatureSearch/Scene/SearchHistoryView/SearchHistoryView.swift
@@ -0,0 +1,22 @@
+//
+//  SearchHistoryView.swift
+//  FeatureSearch
+//
+//  Created by Eric Lee on 2023/10/20.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchHistoryView: View {
+    var body: some View {
+        ZStack {
+            Color(.blue)
+            Text("SearchHistoryView")
+        }
+    }
+}
+
+#Preview {
+    SearchHistoryView()
+}
---
+++ Projects/FeatureSearch/Scene/SearchResultView/SearchResultView.swift
@@ -0,0 +1,22 @@
+//
+//  SearchResultView.swift
+//  FeatureSearch
+//
+//  Created by Eric Lee on 2023/10/20.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchResultView: View {
+    var body: some View {
+        ZStack {
+            Color(.red)
+            Text("SearchResultView")
+        }
+    }
+}
+
+#Preview {
+    SearchResultView()
+}
