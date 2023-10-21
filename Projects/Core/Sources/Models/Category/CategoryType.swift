c6fe5f05a3964c9829d1d9729fc804cdb1ff65a4
sunghun <leedool3003@gmail.com>
2023-10-21 21:25:01 +0900

    :hammer: [chore] Category 모델 추가 #13

---
+++ Projects/Core/Sources/Models/Category/CategoryType.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryType.swift
+//  Core
+//
+//  Created by Kim SungHun on 2023/10/21.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import Foundation
+
+public enum CategoryType {
+	case characteristics
+	case price
+	case region
+}
