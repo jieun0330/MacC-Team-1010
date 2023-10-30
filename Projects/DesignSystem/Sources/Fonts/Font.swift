ccda1e6b508d7d676b084c0dc5407d555a22b38c
mininim <minsoplee2015@gmail.com>
2023-10-30 20:28:54 +0900

    ✨ [feat] 폰트 시스템 구현 #27

---
+++ Projects/DesignSystem/Sources/Fonts/Font.swift
@@ -0,0 +1,23 @@
+//
+//  Font.swift
+//  DesignSystem
+//
+//  Created by Eric Lee on 2023/10/30.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import Foundation
+
+public enum TENTENFont {
+	
+	case SFTitle
+	case SF24B
+	case SF22B
+	case SF17R
+	case SF14R
+	case SF12B
+	case SF12R
+	case SF10B
+	case SF16R
+	
+}
