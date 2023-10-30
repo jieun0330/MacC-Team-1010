2c9b05bfd37489c457d3e6eca2f7f29aa87dc07b
Jieun <jieun_0330@naver.com>
2023-10-30 21:25:09 +0900

    ✨ [feat] EncyclopediaView UI 구현 #26

--- Projects/FeatureEncyclopedia/Scene/EncyclopediaView.swift
+++ Projects/FeatureEncyclopedia/Scene/EncyclopediaView.swift
@@ -7,14 +7,64 @@
 //
 
 import SwiftUI
+import DesignSystem
+
+public struct Model: Identifiable {
+    public let id = UUID()
+    var product: String
+    var title: String
+}
+
+extension Model {
+    public static var dummyModel: [Model] {
+        (0...30).map { (number: Int) in
+            Model(product: "임시", title: "몰라")
+        }
+    }
+}
 
 public struct EncyclopediaView: View {
 	public init() { }
+    
+    public var dummyModel = Model.dummyModel
 	
 	public var body: some View {
-		NavigationStack {
-			Text("EncyclopediaView")
-		}
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.fixed(110)), GridItem(.fixed(110)), GridItem(.fixed(110))], content: {
+                ForEach(dummyModel, content: { (dataItem: Model) in
+                    
+                    Rectangle()
+//                        .foregroundColor(Color(uiColor: .designSystem(.darkgrey)!))
+                        .cornerRadius(10)
+                        .frame(height: 190)
+                        .overlay(
+                            VStack {
+                                
+                                HStack {
+                                    Image(systemName: "ellipsis.message.fill")
+                                    Image(systemName: "heart.fill")
+                                }
+                                
+                                Image("MockMakgeolli")
+                                    
+                                
+//                                Circle()
+//                                    .frame(width: 100)
+//                                    .foregroundColor(.black)
+//                                Text(dataItem.product)
+//                                Text(dataItem.title)
+                            }
+                        )
+                    
+//                    Rectangle()
+//                        .foregroundColor(.gray)
+//                        .overlay(
+//                            Text(dataItem.product)
+//                            Text(dataItem.title)
+//                        )
+                })
+            })
+        }
 	}
 }
 
