8285fcc00977d74b6145767228ab0154db3d9c74
sunghun <leedool3003@gmail.com>
2023-10-31 00:15:58 +0900

    :lipstick: [style] score image set 추가 #24

--- Projects/DesignSystem/Sources/Images/Asset.swift
+++ Projects/DesignSystem/Sources/Images/Asset.swift
@@ -12,7 +12,6 @@ public enum TENTENAsset {
 	/// 에셋의 이름
 	/// - 에셋 추가시 case와 case 에 따른 description을 추가해야합니다.
 	public enum name {
-		
 		case mockMakgeolli
 		
 		case additive
@@ -21,6 +20,25 @@ public enum TENTENAsset {
 		case sour
 		case sweet
 		case thick
+		
+		case scorelarge0
+		case scorelarge1
+		case scorelarge2
+		case scorelarge3
+		case scorelarge4
+		case scorelarge5
+		case scoremini0
+		case scoremini1
+		case scoremini2
+		case scoremini3
+		case scoremini4
+		case scoremini5
+		case scoresmall0
+		case scoresmall1
+		case scoresmall2
+		case scoresmall3
+		case scoresmall4
+		case scoresmall5
 
 		public var description: String {
 			switch self {
@@ -39,6 +57,43 @@ public enum TENTENAsset {
 				return "sweet"
 			case .thick:
 				return "thick"
+				
+			case .scorelarge0:
+				return "scorelarge0"
+			case .scorelarge1:
+				return "scorelarge1"
+			case .scorelarge2:
+				return "scorelarge2"
+			case .scorelarge3:
+				return "scorelarge3"
+			case .scorelarge4:
+				return "scorelarge4"
+			case .scorelarge5:
+				return "scorelarge5"
+			case .scoremini0:
+				return "scoremini0"
+			case .scoremini1:
+				return "scoremini1"
+			case .scoremini2:
+				return "scoremini2"
+			case .scoremini3:
+				return "scoremini3"
+			case .scoremini4:
+				return "scoremini4"
+			case .scoremini5:
+				return "scoremini5"
+			case .scoresmall0:
+				return "scoresmall0"
+			case .scoresmall1:
+				return "scoresmall1"
+			case .scoresmall2:
+				return "scoresmall2"
+			case .scoresmall3:
+				return "scoresmall3"
+			case .scoresmall4:
+				return "scoresmall4"
+			case .scoresmall5:
+				return "scoresmall5"
 			}
 		}
 	}
