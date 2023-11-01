eb99e81f9f24a408d99ff43f3c7afeb94947f864
sunghun <leedool3003@gmail.com>
2023-11-02 04:49:35 +0900

    :hammer: [chore] error 폴더 init #38

---
+++ Projects/Core/Sources/Error/FirebaseError.swift
@@ -0,0 +1,13 @@
+//
+//  FirebaseError.swift
+//  Core
+//
+//  Created by Kim SungHun on 2023/11/02.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import Foundation
+
+public enum FirebaseError: LocalizedError, Equatable {
+	
+}
---
+++ Projects/Core/Sources/Error/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  Core
+//
+//  Created by Kim SungHun on 2023/11/02.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import Foundation
+
+public enum NetworkError: LocalizedError, Equatable {
+	
+}
