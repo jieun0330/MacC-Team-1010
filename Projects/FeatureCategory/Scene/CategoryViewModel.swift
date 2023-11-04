9dbd8031d93087a3ef4de844632d43f556722c99
sunghun <leedool3003@gmail.com>
2023-11-05 01:35:20 +0900

    :sparkles: [feat] 중복 선택 시 mock 데이터 바뀌는 기능 추가 #45

--- Projects/FeatureCategory/Scene/CategoryViewModel.swift
+++ Projects/FeatureCategory/Scene/CategoryViewModel.swift
@@ -27,11 +27,42 @@ final class CategoryViewModel: ObservableObject {
 		fetchLoading = true
 		Task {
 			do {
-//				let stringCategory = categories.map { $0.rawValue }
-//				let response = try await makgeolliRepository.fetchMakgeolliList(
-//					categories: stringCategory.isEmpty ? nil : stringCategory
-//				)
-//				makgeollis = (response.result?.contents)!
+				// 서버 통신 data
+				//				let stringCategory = categories.map { $0.rawValue }
+				//				let response = try await makgeolliRepository.fetchMakgeolliList(
+				//					categories: stringCategory.isEmpty ? nil : stringCategory
+				//				)
+				//				makgeollis = (response.result?.contents)!
+				
+				// mock data
+				var categories = categories
+				if let index = categories.firstIndex(where: {$0 == .additive}) {
+					categories.remove(at: index)
+					// 아스파탐이 없는 true
+				} else {
+					// 아스파탐이 없는 false
+				}
+				let stringCategory = categories.map { $0.scoreDescription }
+				if stringCategory.isEmpty {
+					makHolys = MakHolyMini.mokDatas
+				} else {
+					makHolys = MakHolyMini.mokDatas
+					let filteredMakHolys = makHolys.filter { makHoly in
+						return stringCategory.allSatisfy { category in
+							if category == "sweetness" {
+								return makHoly.sweetness >= 3
+							} else if category == "sourness" {
+								return makHoly.sourness >= 3
+							} else if category == "thickness" {
+								return makHoly.thickness >= 3
+							} else if category == "freshness" {
+								return makHoly.freshness >= 3
+							}
+							return false
+						}
+					}
+					makHolys = filteredMakHolys
+				}
 				fetchLoading = false
 			} catch {
 				// error
