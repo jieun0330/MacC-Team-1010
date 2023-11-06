9e1b604209da796729616cc9986eed2113690fb8
mininim <minsoplee2015@gmail.com>
2023-11-06 16:57:44 +0900

    🔨 [chore] VisibleComment content 프로퍼티 추가 #29

--- Projects/Core/Sources/Models/MakHoly/Reaction/VisibleComment.swift
+++ Projects/Core/Sources/Models/MakHoly/Reaction/VisibleComment.swift
@@ -17,12 +17,19 @@ public struct VisibleComment: Identifiable, Hashable {
 	public let userName: String
 	/// 좋아요 여부
 	public let isLiked: LikeState
+	/// 리뷰 내용
+	public let content: String
 	/// 코멘트 작성 날짜
 	public let date: String
 	
-	public init(userName: String, isLiked: Bool?, date: String) {
+	public init(
+		userName: String,
+		isLiked: LikeState,
+		content: String,
+		date: String) {
 		self.userName = userName
-		self.isLiked = .init(isLiked)
+		self.isLiked = isLiked
+		self.content = content
 		self.date = date
 	}
 	
