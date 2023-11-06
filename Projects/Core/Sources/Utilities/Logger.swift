bc288efbb52d7dac8a259f69b7d6bc49b8861ea0
sunghun <leedool3003@gmail.com>
2023-11-06 14:45:16 +0900

    :sparkles: [feat] Logger 추가 #51

--- Projects/Core/Sources/Utilities/Logger.swift
+++ Projects/Core/Sources/Utilities/Logger.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+public enum Logger {
+	public static func debug(
+		error: Error,
+		message: String,
+		file: String = #file,
+		function: String = #function,
+		line: Int = #line
+	) {
+		let file = file.components(separatedBy: ["/"]).last ?? ""
+		print("\(Date().toString(.log)) [⛔️][\(file)][\(function)][\(line)] -> \(message)")
+		print(error.localizedDescription)
+		debugPrint(error)
+	}
+}
