cd763637ed1bab085e41ec13536b5f2c61a9fc73
mininim <minsoplee2015@gmail.com>
2023-11-06 16:02:19 +0900

    ✨ [feat] MyComment 타입 구현 #29
    MakHoly 데이터 타입 내부에서 사용

---
+++ Projects/Core/Sources/Models/MakHoly/Reaction/MyComment.swift
@@ -0,0 +1,22 @@
+//
+//  MyComment.swift
+//  Core
+//
+//  Created by Eric Lee on 11/6/23.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import Foundation
+
+public struct MyComment: Identifiable, Hashable {
+	
+	/// 코멘트 ID
+	public let id: String
+	/// 공개 여부
+	public var isVisible: Bool
+	/// 코멘트 내용
+	public var contents: String
+	///  코멘트 작성 날짜
+	public var date: String
+	
+}
