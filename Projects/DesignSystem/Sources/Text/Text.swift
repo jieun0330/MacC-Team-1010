8507341ce9e097e43d48a0b8ca6374e255ad9cd4
mininim <minsoplee2015@gmail.com>
2023-11-07 20:31:40 +0900

    🔨 [chore] 폰트 디자인시스템 찐최종 #53

--- Projects/DesignSystem/Sources/Text/Text.swift
+++ Projects/DesignSystem/Sources/Text/Text.swift
@@ -9,71 +9,66 @@
 import SwiftUI
 
 extension Text {
-	func customTextStyle(fontSize : CGFloat, fontWeight: Font.Weight, lineSpacingPer: CGFloat) -> some View {
+	
+	func customTextStyle(fontSize : CGFloat, fontWeight: Font.Weight, perLineHeight: CGFloat) -> some View {
 		self
 			.font(.system(size: fontSize, weight: fontWeight))
-			.lineSpacing(fontSize / 2 * (lineSpacingPer - 100)/100)
+			.lineSpacing(fontSize * (perLineHeight - 100)/100)
 	}
 	
-	func customTextStyle(fontSize : CGFloat, fontWeight: Font.Weight, lineSpacing: CGFloat) -> some View {
+	func customTextStyle(fontSize : CGFloat, fontWeight: Font.Weight, customLineHeight: CGFloat) -> some View {
 		self
 			.font(.system(size: fontSize, weight: fontWeight))
-			.lineSpacing(lineSpacing)
+			.lineSpacing(customLineHeight - fontSize)
 	}
 }
 
 extension Text {
 	
-	public func SFTitle() -> some View {
-		customTextStyle(fontSize: 34, fontWeight: .bold, lineSpacingPer: 0)
-	}
-	
+	// MARK: - Title
 	public func SF24B() -> some View {
-		customTextStyle(fontSize: 24, fontWeight: .bold, lineSpacingPer: 0)
+		customTextStyle(fontSize: 24, fontWeight: .bold, perLineHeight: 140)
 	}
 	
-	public func SF22B() -> some View {
-		customTextStyle(fontSize: 22, fontWeight: .bold, lineSpacingPer: 150)
+	public func SF20B() -> some View {
+		customTextStyle(fontSize: 22, fontWeight: .bold, perLineHeight: 140)
+	}
+	
+	public func SF17B() -> some View {
+		customTextStyle(fontSize: 17, fontWeight: .bold, customLineHeight: 22)
 	}
 	
 	public func SF12B() -> some View {
-		customTextStyle(fontSize: 12, fontWeight: .bold, lineSpacingPer: 150)
+		customTextStyle(fontSize: 12, fontWeight: .bold, perLineHeight: 150)
 	}
 	
 	public func SF10B() -> some View {
-		customTextStyle(fontSize: 10, fontWeight: .bold, lineSpacingPer: 0)
+		customTextStyle(fontSize: 10, fontWeight: .bold, perLineHeight: 140)
 	}
 	
-	public func SF14R() -> some View {
-		customTextStyle(fontSize: 14, fontWeight: .regular, lineSpacingPer: 140)
-	}
-	
-	public func SF12R() -> some View {
-		customTextStyle(fontSize: 12, fontWeight: .regular, lineSpacingPer: 140)
-	}
-	
+	// MARK: - Title
 	public func SF17R() -> some View {
-		customTextStyle(fontSize: 17, fontWeight: .regular, lineSpacingPer: 17)
+		customTextStyle(fontSize: 17, fontWeight: .regular, customLineHeight: 22)
 			.padding(.horizontal, 1000)
 			.kerning(-0.4)
 			.drawingGroup()
 			.padding(.horizontal, -1000)
 	}
-	
-	public func SF16R() -> some View {
-		customTextStyle(fontSize: 16, fontWeight: .regular, lineSpacing: 18)
+
+	public func SF15R() -> some View {
+		customTextStyle(fontSize: 15, fontWeight: .regular, customLineHeight: 18)
 	}
 	
-	public func SF28B() -> some View {
-		customTextStyle(fontSize: 28, fontWeight: .bold, lineSpacing: 34)
+	public func SF14R() -> some View {
+		customTextStyle(fontSize: 14, fontWeight: .regular, perLineHeight: 140)
 	}
 	
-	public func SF17B() -> some View {
-		customTextStyle(fontSize: 17, fontWeight: .bold, lineSpacing: 34)
+	public func SF12R() -> some View {
+		customTextStyle(fontSize: 12, fontWeight: .regular, perLineHeight: 140)
 	}
 	
-	public func SF20B() -> some View {
-		customTextStyle(fontSize: 20, fontWeight: .bold, lineSpacing: 22)
+	public func SF10R() -> some View {
+		customTextStyle(fontSize: 20, fontWeight: .regular, perLineHeight: 140)
 	}
 	
 }
