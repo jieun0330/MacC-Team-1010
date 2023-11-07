36320eb5de5e07e346404d583b711ca77b464d56
mininim <minsoplee2015@gmail.com>
2023-11-07 11:08:45 +0900

    💄 [style] InfoDescriptionView UI 구현 #29

---
+++ Projects/FeatureInformation/Scene/Components/InfoDescriptionView.swift
@@ -0,0 +1,37 @@
+//
+//  InfoDescriptionView.swift
+//  FeatureInformation
+//
+//  Created by Eric Lee on 11/6/23.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct InfoDescriptionView: View {
+	@ObservedObject var viewModel: InformationViewModel
+	
+    var body: some View {
+		VStack(alignment: .leading, spacing: 0) {
+			
+			HStack {
+				Text("소개")
+					.font(.style(.SF20B))
+					.foregroundColor(Color(uiColor: .designSystem(.white)!))
+				Spacer()
+			}
+			.padding(.vertical, 20)
+			
+			Text(viewModel.makHoly?.description ?? "")
+				.font(.style(.SF14R))
+				.foregroundColor(Color(uiColor: .designSystem(.w85)!))
+				.multilineTextAlignment(.leading)
+				.lineLimit(nil)
+				.fixedSize(horizontal: false, vertical: true)
+				.padding(.bottom, 20)
+			
+		}
+    }
+}
+
