b9c7621768a60c2ffe30f78cae06b2331385056b
mininim <minsoplee2015@gmail.com>
2023-11-08 23:59:58 +0900

    ✨ [feat] InfoNameView 구현 #29

---
+++ Projects/FeatureInformation/Scene/InformationCardView/InfoNameView.swift
@@ -0,0 +1,25 @@
+//
+//  InfoNameView.swift
+//  FeatureInformation
+//
+//  Created by Eric Lee on 11/8/23.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct InfoNameView: View {
+	let name: String
+	let description: String
+    var body: some View {
+		VStack(spacing: 0) {
+			Text(name)
+				.SF24B()
+				.foregroundColor(.White)
+			Text(description)
+				.SF15R()
+				.foregroundColor(.W50)
+		}
+    }
+}
