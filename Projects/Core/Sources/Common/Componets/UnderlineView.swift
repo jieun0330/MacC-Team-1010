89bdead2d7112c4dea2198eb7fc868aac43b7183
sunghun <leedool3003@gmail.com>
2023-11-09 02:42:21 +0900

    :hammer: [chore] Underline Component View 추가 #58

---
+++ Projects/Core/Sources/Common/Componets/UnderlineView.swift
@@ -0,0 +1,40 @@
+//
+//  UnderlineView.swift
+//  Core
+//
+//  Created by Kim SungHun on 2023/11/09.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import SwiftUI
+
+public struct UnderlineView: ViewModifier {
+	public let spacing: CGFloat
+	public let height: CGFloat
+	public let color: Color
+	
+	init(spacing: CGFloat, height: CGFloat, color: Color) {
+		self.spacing = spacing
+		self.height = height
+		self.color = color
+	}
+	
+	public func body(content: Content) -> some View {
+		VStack(spacing: spacing) {
+			content
+			Rectangle()
+				.frame(height: height)
+				.foregroundColor(color)
+		}
+	}
+}
+
+extension View {
+	public func underlined(spacing: CGFloat = 3,
+						   height: CGFloat = 2,
+						   color: Color = .accentColor) -> some View {
+		self.modifier(UnderlineView(spacing: spacing,
+									height: height,
+									color: color))
+	}
+}
