fdba19da9b2cc2e7f818535077feecc8cfb48d2d
sunghun <leedool3003@gmail.com>
2023-11-09 02:37:29 +0900

    :lipstick: [style] SF20B 추가 #58

--- Projects/DesignSystem/Sources/Fonts/Font+.swift
+++ Projects/DesignSystem/Sources/Fonts/Font+.swift
@@ -35,6 +35,8 @@ extension Font {
 			return .system(size: 16, weight: .regular)
 		case .SF15R:
 			return .system(size: 15, weight: .regular)
+		case .SF20B:
+			return .system(size: 20, weight: .bold)
 		}
 		
 	}
