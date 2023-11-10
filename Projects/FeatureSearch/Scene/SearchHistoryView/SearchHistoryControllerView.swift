d2f37da9330759d361c97590ec82149c8beee0a2
sunghun <leedool3003@gmail.com>
2023-11-11 03:09:31 +0900

    :sparkles: [feat] 검색어 지우기 alert 추가 #72

--- Projects/FeatureSearch/Scene/SearchHistoryView/SearchHistoryControllerView.swift
+++ Projects/FeatureSearch/Scene/SearchHistoryView/SearchHistoryControllerView.swift
@@ -12,6 +12,8 @@ import DesignSystem
 struct SearchHistoryControllerView: View {
 	@ObservedObject var searchViewModel: SearchViewModel
 	
+	@State var showAlert = false
+	
 	var body: some View {
 		HStack(alignment: .center, spacing: 0) {
 			Text("최근 검색어")
@@ -21,14 +23,25 @@ struct SearchHistoryControllerView: View {
 			Spacer()
 			
 			Button(action: {
-				searchViewModel.clearSearchHistory()
+				showAlert = true
 			}, label: {
 				Text("지우기")
 					.font(.style(.SF14R))
 					.foregroundColor(Color(uiColor: .designSystem(.primary)!))
 			})
 		}
+		.alert(isPresented: $showAlert) {
+			Alert(title: Text("최근 검색어 지우기"), message: Text("검색한 기록을 모두 지울까요?"),
+				  primaryButton: .cancel(
+					Text("취소"),
+					action: {}
+				  ), secondaryButton: .default(
+					Text("지우기"),
+					action: {
+						searchViewModel.clearSearchHistory()
+					}
+				  ))
+		}
 		.padding(.vertical, 10)
-		
 	}
 }
