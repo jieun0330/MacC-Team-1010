a2725aaa432f957c9a7920901b284fa4a55e685f
sunghun <leedool3003@gmail.com>
2023-11-14 20:52:06 +0900

    :hammer: [chore] date -> string extension 추가 #82

---
+++ Projects/Core/Sources/Common/Extensions/String+.swift
@@ -0,0 +1,26 @@
+//
+//  String+.swift
+//  Core
+//
+//  Created by Kim SungHun on 2023/11/14.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+	func extractDateFromISOString() -> String? {
+		let components = self.components(separatedBy: "T")
+		guard let dateString = components.first else {
+			return nil
+		}
+		let dateComponents = dateString.components(separatedBy: "-")
+		guard dateComponents.count == 3,
+			  let year = dateComponents.first,
+			  let month = dateComponents.dropFirst().first,
+			  let day = dateComponents.dropFirst(2).first else {
+			return nil
+		}
+		return "\(year)년 \(month)월 \(day)일"
+	}
+}
