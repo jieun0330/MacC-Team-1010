28f56fe9af60feb6ad02be2c93ce0d350d54c5ed
mininim <minsoplee2015@gmail.com>
2023-11-15 23:49:07 +0900

    💄 [style] SearchView background Color 추가 #88

--- Projects/FeatureSearch/Scene/SearchView.swift
+++ Projects/FeatureSearch/Scene/SearchView.swift
@@ -18,33 +18,35 @@ public struct SearchView: View {
 	
 	public var body: some View {
 		NavigationStack {
-			SearchSuggestionView(searchViewModel: searchViewModel)
-				.searchable(
-					text: $searchViewModel.searchText,
-					prompt: "막걸리 이름 ..."
-				)
-				.onChange(of: searchViewModel.searchText) { newValue in
-					searchViewModel.searchState = true
-					searchViewModel.fetchLoading = true
-				}
-				.onSubmit(of: .search) {
-					searchViewModel.searchState = false
-					searchViewModel.searchMakHolies(searchText: searchViewModel.searchText)
-				}
-				.onAppear {
-					UIBarButtonItem.appearance(
-						whenContainedInInstancesOf: [UISearchBar.self]).title = "취소"
-					UISearchBar.appearance().tintColor = .designSystem(.primary)
-				}
-				.background(Color(uiColor: .designSystem(.darkbase)!))
-				.alert(isPresented: $searchViewModel.errorState) {
-					Alert(title: Text("네트워크 에러"), message: Text("인터넷 연결상태를 확인해주세요."),
-						  dismissButton: .default(Text("확인")))
-				}
-				.toolbarBackground(
-					Color.DarkBase,
-					for: .navigationBar
-				)
+			ZStack {
+				Color.DarkBase.ignoresSafeArea()
+				SearchSuggestionView(searchViewModel: searchViewModel)
+			}
+			.alert(isPresented: $searchViewModel.errorState) {
+				Alert(title: Text("네트워크 에러"), message: Text("인터넷 연결상태를 확인해주세요."),
+					  dismissButton: .default(Text("확인")))
+			}
+			.searchable(
+				text: $searchViewModel.searchText,
+				prompt: "막걸리 이름 ..."
+			)
+			.onChange(of: searchViewModel.searchText) { newValue in
+				searchViewModel.searchState = true
+				searchViewModel.fetchLoading = true
+			}
+			.onSubmit(of: .search) {
+				searchViewModel.searchState = false
+				searchViewModel.searchMakHolies(searchText: searchViewModel.searchText)
+			}
+			.onAppear {
+				UIBarButtonItem.appearance(
+					whenContainedInInstancesOf: [UISearchBar.self]).title = "취소"
+				UISearchBar.appearance().tintColor = .designSystem(.primary)
+			}
+			.toolbarBackground(
+				Color.W10,
+				for: .navigationBar
+			)
 		}
 	}
 }
