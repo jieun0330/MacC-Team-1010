013611840e4347346c799321e0b160cbc23fc1c2
mininim <minsoplee2015@gmail.com>
2023-11-21 17:38:17 +0900

    🔨 [chore] Mixpanel 의존성 추가 #120

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -12,7 +12,8 @@ let dependencies = Dependencies(
 	carthage: [],
 	swiftPackageManager: [
 		.moya,
-		.kingfisher
+		.kingfisher,
+		.mixpanel
 	],
 	platforms: [.iOS]
 )
@@ -20,4 +21,5 @@ let dependencies = Dependencies(
 fileprivate extension Package {
 	static let moya: Self = .remote(url: "https://github.com/Moya/Moya", requirement: .upToNextMajor(from: "15.0.0"))
 	static let kingfisher: Self = .remote(url: "https://github.com/onevcat/Kingfisher.git", requirement: .upToNextMajor(from: "7.0.0"))
+	static let mixpanel: Self = .remote(url: "https://github.com/mixpanel/mixpanel-swift", requirement: .branch("master"))
 }
--- Tuist/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Tuist/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -14,4 +14,5 @@ public extension TargetDependency {
 public extension TargetDependency.SPM {
 	static let Kingfisher = TargetDependency.external(name: "Kingfisher")
 	static let Moya = TargetDependency.external(name: "Moya")
+	static let Mixpanel = TargetDependency.external(name: "Mixpanel")
 }
