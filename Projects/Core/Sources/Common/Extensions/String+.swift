9be980a37cb3b4c0aa391c84265bb29de78a08e5
sunghun <leedool3003@gmail.com>
2023-11-27 01:27:20 +0900

    :hammer: [chore] validatePhone extension 추가 #129

--- Projects/Core/Sources/Common/Extensions/String+.swift
+++ Projects/Core/Sources/Common/Extensions/String+.swift
@@ -27,4 +27,10 @@ public extension String {
 	func removeTrailingSpaces() -> String {
 		return self.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression)
 	}
+	
+	func validatePhone(number: String) -> Bool {
+		let regex = "^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$"
+		return NSPredicate(format: "SELF MATCHES %@", regex)
+			.evaluate(with: number)
+	}
 }
