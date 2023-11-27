820d189c78e3b15684b4b8075a2b2260d990cbf8
sunghun <leedool3003@gmail.com>
2023-11-27 15:32:06 +0900

    :recycle: [refactor] alert 문구 변경 #131

--- Projects/FeatureOnboarding/Scene/Auth/GenderView.swift
+++ Projects/FeatureOnboarding/Scene/Auth/GenderView.swift
@@ -72,10 +72,10 @@ public struct GenderView: View {
 		.alert(isPresented: $showAlert) {
 			Alert(title: Text("막걸리 정보를 보관할 수 없어요"),
 				  message: Text("번호를 입력하지 않으면 기기 변동 시 내 막걸리 정보를 불러올 수 없어요"),
-				  primaryButton: .cancel(Text("안하기")) {
+				  primaryButton: .cancel(Text("건너뛰기")) {
 				isSkip = true
 			},
-				  secondaryButton: .default(Text("보관하기")))
+				  secondaryButton: .default(Text("취소")))
 		}
 		.modifier(OnboardingBackground())
 		.fullScreenCover(isPresented: $isSkip, content: {
