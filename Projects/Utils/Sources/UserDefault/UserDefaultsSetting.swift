d4f436fa610dd66c036dcade01ed7d53c859e188
mininim <minsoplee2015@gmail.com>
2023-11-29 23:04:34 +0900

    🔨 [chore] 다시 추가 #120

---
+++ Projects/Utils/Sources/UserDefault/UserDefaultsSetting.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultsSetting.swift
+//  Utils
+//
+//  Created by Kim SungHun on 2023/11/09.
+//  Copyright © 2023 com.tenten. All rights reserved.
+//
+
+import Foundation
+
+public enum UserDefaultsSetting {
+	
+}
