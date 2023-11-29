1c2d60aa9a9c7c688ef0fdb3662ab4d571f060d6
mininim <minsoplee2015@gmail.com>
2023-11-29 21:25:17 +0900

    ✨ [feat] 가입 시작 추적 #120

--- Projects/FeatureOnboarding/Scene/Auth/PhoneNumberAuthView.swift
+++ Projects/FeatureOnboarding/Scene/Auth/PhoneNumberAuthView.swift
@@ -127,6 +127,9 @@ public struct PhoneNumberAuthView: View {
 						.opacity(0.5)
 				}
 			}
+			.onAppear {
+				MixpanelManager.shared.signupStart()
+			}
 			.navigationBarItems(trailing: Button(action: {
 				viewModel.alertItem = AlertItem(title: Text("막걸리 정보를 보관할 수 없어요"),
 												message: Text("번호를 입력하지 않으면 기기 변동 시 내 막걸리 정보를 불러올 수 없어요"),
