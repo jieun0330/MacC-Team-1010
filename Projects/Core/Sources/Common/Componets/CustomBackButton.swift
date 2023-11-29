b9127cb10d7bc3331e8ebbf901913bd26e112f97
sunghun <leedool3003@gmail.com>
2023-11-29 17:12:39 +0900

    :hammer: [chore] navi back 로직 변경 및 특징으로 찾기 뷰 적용 #131

--- Projects/Core/Sources/Common/Componets/CustomBackButton.swift
+++ Projects/Core/Sources/Common/Componets/CustomBackButton.swift
@@ -9,32 +9,28 @@
 import SwiftUI
 import DesignSystem
 
-//public struct CustomBackButton: View {
-//	@Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-//
-//	let title: String
-//
-//	public init(title: String = "돌아가기") { self.title = title }
-//
-//	public var body: some View {
-//		Button(action: {
-//			self.presentationMode.wrappedValue.dismiss()
-//		}) {
-//			HStack(spacing: 3) {
-//				Image(systemName: "chevron.left")
-//				Text(title)
-//			}
-//		}
-//	}
-//}
-//
-//extension UINavigationController: UIGestureRecognizerDelegate {
-//	override open func viewDidLoad() {
-//		super.viewDidLoad()
-//		interactivePopGestureRecognizer?.delegate = self
-//	}
-//
-//	public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-//		return viewControllers.count > 1
-//	}
-//}
+public struct CustomBackButton: View {
+	@Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
+	let title: String
+
+	public init(title: String = "돌아가기") { self.title = title }
+
+	public var body: some View {
+		Button(action: {
+			self.presentationMode.wrappedValue.dismiss()
+		}) {
+			HStack(spacing: 3) {
+				Image(systemName: "chevron.left")
+				Text(title)
+			}
+		}
+	}
+}
+
+extension UINavigationController {
+  open override func viewWillLayoutSubviews() {
+	super.viewWillLayoutSubviews()
+	  navigationBar.topItem?.backButtonDisplayMode = .minimal
+  }
+}
